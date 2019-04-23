7fcfb9a22a4ed82d4a566344ed505ba5e3a67d4c
Sulejman Ljimaj <sulejman-limaj@hotmail.be>
2019-04-23 08:38:27 +0000

    added styling to several pages, delted a table and made minor changes in views

--- detail/views.py
+++ detail/views.py
@@ -2,6 +2,8 @@ import csv
 from django.shortcuts import render, get_object_or_404, redirect, reverse, HttpResponse
 from users.models import Pictures, CustomUser
 from home.views import get_all_images
+from django.contrib.auth.models import User
+from users.models import CustomUser
 
 def detail_image(request, pk):
     image = get_object_or_404(Pictures, pk=pk)
@@ -11,5 +13,5 @@ def detail_image(request, pk):
     if request.method == 'POST':
         image.likes +=1
         image.save()
-        
+
     return render(request, 'detail/detail.html', {'image': image})
