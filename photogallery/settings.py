05f1f32dfaf830ac9672ceea04ae6c7efd449acc
Sulejman Ljimaj <sulejman-limaj@hotmail.be>
2019-04-25 07:51:47 +0000

    initial commit

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -161,8 +161,8 @@ MEDIA_URL = '/media/'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
-STRIPE_PUBLISHABLE_KEY = "pk_test_ZOQMm0n0Y3OCoxznx8ufeunq00mXlHWdJX"
-STRIPE_SECRET_KEY = "sk_test_cFrwY9eSokJOtvTs51UlzouH00Sff0lpP0"
+STRIPE_PUBLISHABLE_KEY = os.environ.get("STRIPE_PUBLISHABLE_KEY")
+STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 
 
 
