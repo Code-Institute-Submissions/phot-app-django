9d158f3af42dace599e3fcca7518d247486b5b97
Sulejman Ljimaj <sulejman-limaj@hotmail.be>
2019-04-29 09:24:47 +0000

    added host from heroku  to allowed hosts

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['photo-gallery-mls-project-geronimo1992.c9users.io']
+ALLOWED_HOSTS = ['photo-gallery-mls-project-geronimo1992.c9users.io', 'photogallery-milestone-project.herokuapp.com']
 
 
 # Application definition
