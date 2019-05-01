01d0868dc906fd11a2c3cc17f8a9e1893f678b19
Sulejman Ljimaj <sulejman-limaj@hotmail.be>
2019-05-01 03:38:25 +0000

    inor change

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -150,7 +150,7 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
-#other mail settings
+#other mail
 EMAIL_HOST = "smtp.sendgird.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
