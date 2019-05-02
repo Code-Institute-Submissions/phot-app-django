390725e727f780148b7b41d38bfefc4634ea9eeb
Sulejman Ljimaj <sulejman-limaj@hotmail.be>
2019-05-02 09:25:29 +0000

    reposition of like form

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -178,7 +178,7 @@ STATICFILES_DIRS = (
 
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-import env 
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
